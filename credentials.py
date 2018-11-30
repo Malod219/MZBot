99f335c2a9482731f8df1cf89bebe59e1b2e8a8f
aaron <perf3ctodst@gmail.com>
2018-11-30 21:46:52 +0000

    Add files via upload
    Uploaded Rewrite 1

--- credentials.py
+++ credentials.py
@@ -1,14 +1,13 @@
-#Twitter
-consumer_key='CatchMeIfYouCan'
-consumer_secret='The Cure'
-access_token_key='Gorillaz'
-access_token_secret='Shrek'
-#Discord
-bot_token='Rodents Of Unusual Size'
-#Reddit
+import os
 
-client_id='Sun Tzu'
-client_secret='Vowel'
-username='Heh'
-password="asdf"
-user_agent="asdfwarrior"
+# Your Discord Bot Token
+bot_token = os.environ.get("DISCORD_BOT_TOKEN")
+# Your Reddit User Agent
+user_agent = os.environ.get("REDDIT_USER_AGENT")
+client_id = os.environ.get("REDDIT_ID")
+client_secret = os.environ.get("REDDIT_SECRET")
+# Twitter keys
+consumer_key = os.environ.get("TWITTER_CONSUMER_KEY")
+consumer_secret = os.environ.get("TWITTER_CONSUMER_SECRET")
+access_token_key = os.environ.get("TWITTER_ACCESS_TOKEN")
+access_token_secret = os.environ.get("TWITTER_ACCESS_KEY")
