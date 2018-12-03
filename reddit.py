13fd9925b31c18965004b0131669497cb157545a
aaron <perf3ctodst@gmail.com>
2018-12-03 21:39:32 +0000

    Update reddit.py
    Modified reddit.py
    Following features have been added:
    - Config file to set subreddit to get new posts from
    - Config file to set the ID for the channel the bot posts in
    This removes the need for a txt file containing channels.
    The txt file for last post is removed, as it exists for the startup to make sure it doesn't spam posts, but a simple time of post against time of startup fixes that.

--- reddit.py
+++ reddit.py
@@ -3,106 +3,62 @@ import discord
 import asyncio
 from discord.ext import commands
 import credentials
+import time
 
-reddit = praw.Reddit(
-    client_id = credentials.client_id,
-    client_secret=credentials.client_secret,
-    user_agent = credentials.user_agent
-    )
+with open("config.cfg", "r") as configFile:
+    config = configFile.readlines()
+
 
 class RedditCog:
-    def __init__(self,bot):
-        
-        self.bot = bot
-        self.bot.enabled=True
-        try:
-            with open('PreviousReddit.txt','r'):
-                pass
-        except:
-            with open('PreviousReddit.txt','w'):
-                pass
+    def __init__(self, bot):
 
+        self.bot = bot
+        self.bot.enabled = True
 
     async def on_ready(self):
-        oldTitle='123'
-        title=''
-        while self.bot.enabled==True:
-            subreddit=reddit.subreddit('minez')
-            posts=subreddit.new(limit=1)
-            for post in posts:
-                title=post.title
-                with open('PreviousReddit.txt','r+') as r:
-                    lines=r.readlines()
-                    if title in lines:
-                        same=True
-                    else:
-                        same=False
-                url=post.url
-            if (title!=oldTitle and same==False):
-                with open('RedditChannelPosts.txt','r+') as r:
-                    lines=r.readlines()
-                for line in lines:
-                    line=line.replace('\n','')
-                    channel=self.bot.get_channel(line)
-                    try:
-                        await self.bot.send_message(channel,'```New Reddit Post in r/MineZ:\n'+title+'\nPostURL: '+url+'```')
-                    except:
-                        pass
-                with open('PreviousReddit.txt','w') as r:
-                    r.write(title)
-                oldTitle=title
-            await asyncio.sleep(10)        
 
-    @commands.group(pass_context=True,brief='\n    addchannel\n    removechannel')
-    async def reddit(self,ctx):
+        reddit = praw.Reddit(
+            client_id=credentials.client_id,
+            client_secret=credentials.client_secret,
+            user_agent=credentials.user_agent)
+
+        start_time = time.time()
+        subreddit = reddit.subreddit('minez')
+        postingChannel = self.bot.get_channel(config[2][18:0])
+        currentPost = ""
+        while self.bot.enabled == True:
+            posts = subreddit.new(limit=1)
+            for post in posts:
+                if post == currentPost:
+                    await asyncio.sleep(60)
+                elif post.created_utc > start_time and post.title != currentPost:
+                    await self.bot.send_message(postingChannel, '```New Reddit Post in r/'+config[1][10:]+':\n' + post.title + '\nPostURL: ' + post.url + '```')
+                    currentPost = post.title
+                    await asyncio.sleep(60)
+
+    @commands.group(
+        pass_context=True, brief='\n    addchannel\n    removechannel')
+    async def reddit(self, ctx):
         if ctx.invoked_subcommand is None:
             await self.bot.say('`No valid command entered`')
-            
-    @reddit.command(pass_context=True)
-    async def help(self,ctx):
-        embed = discord.Embed(title="Reddit Commands", description="Reddit Commands are:", color=0xeee657)
 
-        embed.add_field(name=">reddit addchannel", value="Adds current channel to recieve messages", inline=False)
-        embed.add_field(name=">reddit removechannel", value="Removes current channel from recieving messages", inline=False)
+    @reddit.command(pass_context=True)
+    async def help(self, ctx):
+        embed = discord.Embed(
+            title="Reddit Commands",
+            description="Reddit Commands are:",
+            color=0xeee657)
+
+        embed.add_field(
+            name=">reddit addchannel",
+            value="Adds current channel to recieve messages",
+            inline=False)
+        embed.add_field(
+            name=">reddit removechannel",
+            value="Removes current channel from recieving messages",
+            inline=False)
         await self.bot.send_message(ctx.message.channel, embed=embed)
 
 
-    @reddit.command(pass_context=True)
-    async def addchannel(self,ctx):
-        if ctx.message.author.server_permissions.administrator==True:
-            channel=str(ctx.message.channel.id)
-            with open('RedditChannelPosts.txt','w+') as r:
-                if channel in r.read():
-                    print('FOUND!')
-                else:
-                    r.write('\n'+channel)
-            await self.bot.say('Reddit channel added!')
-        else:
-            await self.bot.say("You don't have Administrator permissions for this command")
-
-    @reddit.command(pass_context=True)
-    async def removechannel(self,ctx):
-        if ctx.message.author.server_permissions.administrator==True:
-            output=""
-            channel=str(ctx.message.channel.id)
-
-            with open('RedditChannelPosts.txt','r+') as r:
-                lines=r.readlines()
-                print(lines)
-                for line in lines:
-                    print(lines)
-                    if channel in line:
-                        output+=""
-                    else:
-                        output+=(str(line)+'\n')
-            with open('RedditChannelPosts.txt','w+') as r:
-                r.write(output)
-                await self.bot.say('Removed')
-        else:
-            await self.bot.say("You don't have Administrator permissions for this command")
-
-
-        
 def setup(bot):
     bot.add_cog(RedditCog(bot))
-
